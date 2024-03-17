08d86f414621bb8b62adb4a1c9b5f01fde87aea5
Rylan Lorance <rylancl@gmail.com>
2024-03-17 11:14:18 -0700

    add unit tests

--- unit_test.sh
+++ unit_test.sh
@@ -25,20 +25,20 @@ echo ""
 echo "Step 3. Running File rename tool with safe mode on."
 python app.py rename $INPUT_DIR $OUTPUT_DIR --safe-mode on
 
-echo "Step 4. Safe mode passed, rename the files."
-python app.py rename $INPUT_DIR $OUTPUT_DIR --safe-mode off
+# echo "Step 4. Safe mode passed, rename the files."
+# python app.py rename $INPUT_DIR $OUTPUT_DIR --safe-mode off
 
-echo "lets see if those files were created"
-ls -al $OUTPUT_DIR
+# echo "lets see if those files were created"
+# ls -al $OUTPUT_DIR
 
-echo "moving files to new directory..."
+# echo "moving files to new directory..."
 
-INPUT_DIR_CLEANED="/Users/rylanlorance/Documents/Dear_Ocean/Dear_Ocean_Digital_Coral_Ark_Bot_Working/input/input_files_cleaned_demo_01_08_23/"
-cp $OUTPUT_DIR/* $INPUT_DIR_CLEANED
-INPUT_DIR=$INPUT_DIR_CLEANED
+# INPUT_DIR_CLEANED="/Users/rylanlorance/Documents/Dear_Ocean/Dear_Ocean_Digital_Coral_Ark_Bot_Working/input/input_files_cleaned_demo_01_08_23/"
+# cp $OUTPUT_DIR/* $INPUT_DIR_CLEANED
+# INPUT_DIR=$INPUT_DIR_CLEANED
 
-echo "Step 5. Run Upload Script with Safe Mode On"
-python app.py upload $INPUT_DIR --safe-mode on
+# echo "Step 5. Run Upload Script with Safe Mode On"
+# python app.py upload $INPUT_DIR --safe-mode on
 
-# echo "Passed!"
-python app.py upload $INPUT_DIR --safe-mode off
+# # echo "Passed!"
+# python app.py upload $INPUT_DIR --safe-mode off
